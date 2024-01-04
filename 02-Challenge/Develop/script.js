4ce3f969820932ba2b844c10128f39ae118cb1ed
CooleyJeffrey <151467361+CooleyJeffrey@users.noreply.github.com>
2024-01-04 16:29:25 -0500

    Started may rewrite

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(){
+  var passwordLength = prompt ("Enter password legnth:");
+  passwordLength = parseInt(passwordLength);
+  
+  if (isNaN(passwordLength)|| passwordLength <= 0) {
+    alert("Please enter a valid number for password length.");
+    return "";
+  }
+
+  if (confirm("Lowercase?")) {
+    var lowercase = true;
+    console.log(lowercase);
+  }
+  
+  if (confirm("Uppercase?")) {
+    var uppercase = true;
+    console.log(uppercase);
+  }
+  
+  if (confirm("Numericals?")){
+    var numericals = true;
+    console.log(numericals)
+  }
+
+  if (confirm("Special Characters")){
+    var special = true;
+    console.log(special)
+  }
+
+
+
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
